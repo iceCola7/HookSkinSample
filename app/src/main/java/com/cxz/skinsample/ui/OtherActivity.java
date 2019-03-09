b55b479a3ff5274b3dede9e90392ced80ad98ef7
chenxz <563859095@qq.com>
2019-03-09 22:42:20 +0800

    commit

--- app/src/main/java/com/cxz/skinsample/ui/OtherActivity.java
+++ app/src/main/java/com/cxz/skinsample/ui/OtherActivity.java
@@ -1,13 +1,15 @@
-package com.cxz.skinsample;
+package com.cxz.skinsample.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.cxz.skinsample.R;
+
+public class OtherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_other);
     }
 }
